ff8c04191e1154944b8d81a636e4c938995965a6
machuanhu <1748771273@qq.com>
2021-04-08 23:02:12 +0800

    FEA:the job of part1

---
+++ github-management-system/src/part1/user/User.java
@@ -0,0 +1,24 @@
+package part1.user;
+
+public class User {
+    private String name;
+    private String password;
+
+    public User() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ github-management-system/src/part1/util/JSONUtil.java
@@ -0,0 +1,7 @@
+package part1.util;
+import com.alibaba.fastjson.JSON;
+import java.io.File;
+
+public class JSONUtil {
+
+}
