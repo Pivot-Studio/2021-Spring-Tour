5b0e1acf963eb1a011b024126c38b84d51c01d16
DESKTOP-NJI1K8V\86188 <695693802@>
2021-04-10 00:01:50 +0800

    代码完成网络请求

---
+++ 2.7.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+// 发送GET请求
+// url：         请求地址
+// response：    请求返回的内容
+func Get(url string) string {
+
+	// 超时时间：5秒
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	var buffer [512]byte
+	result := bytes.NewBuffer(nil)
+	for {
+		n, err := resp.Body.Read(buffer[0:])
+		result.Write(buffer[0:n])
+		if err != nil && err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+	}
+
+	return result.String()
+}
+
+func main() {
+	fmt.Println(Get("https://api.github.com/users/defunkt"))
+}
