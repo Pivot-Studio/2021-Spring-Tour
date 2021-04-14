eb145144552de507246ee16529751a7298e048a4
DESKTOP-NJI1K8V\86188 <695693802@>
2021-04-14 23:53:25 +0800

    四月十四日

---
+++ 项目二/stars.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"plz"
+)
+
+func Getstar() string {
+
+	url := "https://api.github.com/users/Destined777/starred"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	req.Header.Add("Authorization", plz.Token())
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	//fmt.Println(string(body))
+	return string(body)
+}
